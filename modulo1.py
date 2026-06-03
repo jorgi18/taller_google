4394eff199f31e29494a638fb2c06b3761bdca5a
Jorge Ivan <jorgeivanortegameza@gmail.com>
2026-06-02 19:37:21 -0600

    git

---
+++ modulo1.py
@@ -0,0 +1,10 @@
+nombre = "Ivan"
+edad = 18
+estatura = 1.75
+Mexicano = True
+print("Hola, mi nombre es", nombre, "y tengo", edad, "años.")
+print("Mi estatura es de", estatura, "metros.")
+print("Soy mexicano:", Mexicano)
+
+ingreso = float(input("Ingrese su ingreso mensual: "))
+print("Su ingreso mensual es de:", ingreso)
